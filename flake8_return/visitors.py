aa70b51f99185298b04ef4be40c5eec33755d2f3
Afonasev Evgeniy <ea.afonasev@gmail.com>
2019-06-20 15:39:13 +0500

    split visitor to some parts

--- flake8_return/visitors.py
+++ flake8_return/visitors.py
@@ -20,11 +20,7 @@ REFS = 'refs'
 RETURNS = 'returns'
 
 
-class ReturnVisitor(Visitor):
-    def __init__(self) -> None:
-        super().__init__()
-        self._stack: List[Any] = []
-
+class UnnecessaryAssignMixin(Visitor):
     @property
     def assigns(self) -> NameToLines:
         return self._stack[-1][ASSIGNS]
@@ -32,28 +28,6 @@ class ReturnVisitor(Visitor):
     @property
     def refs(self) -> NameToLines:
         return self._stack[-1][REFS]
-
-    @property
-    def returns(self) -> List[ast.Return]:
-        return self._stack[-1][RETURNS]
-
-    def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
-        self._visit_with_stask(node)
-
-    def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> None:
-        self._visit_with_stask(node)
-
-    def _visit_with_stask(self, node: Function) -> None:
-        self._stack.append(
-            {ASSIGNS: defaultdict(list), REFS: defaultdict(list), RETURNS: []}
-        )
-        self.generic_visit(node)
-        self._check_function(node)
-        self._stack.pop()
-
-    def visit_Return(self, node: ast.Return) -> None:
-        self.returns.append(node)
-        self.generic_visit(node)
 
     def visit_Assign(self, node: ast.Assign) -> None:
         if not self._stack:
@@ -70,6 +44,10 @@ class ReturnVisitor(Visitor):
 
         self._visit_assign_target(target)
 
+    def visit_Name(self, node: ast.Name) -> None:
+        if self._stack:
+            self.refs[node.id].append(node.lineno)
+
     def _visit_assign_target(self, node: ast.AST) -> None:
         if isinstance(node, ast.Tuple):
             for n in node.elts:
@@ -82,72 +60,6 @@ class ReturnVisitor(Visitor):
 
         # get item, etc.
         self.generic_visit(node)
-
-    def visit_Name(self, node: ast.Name) -> None:
-        if self._stack:
-            self.refs[node.id].append(node.lineno)
-
-    def _check_function(self, node: Function) -> None:
-        if not self.returns or not node.body:
-            return
-
-        if len(node.body) == 1 and isinstance(node.body[-1], ast.Return):
-            # skip functions that consist only `return None`
-            return
-
-        if not self._result_exists():
-            self._check_unnecessary_return_none()
-            return
-
-        self._check_implicit_return_value()
-        self._check_implicit_return(node.body[-1])
-
-        for n in self.returns:
-            if n.value:
-                self._check_unnecessary_assign(n.value)
-
-    def _result_exists(self) -> bool:
-        for node in self.returns:
-            value = node.value
-            if value and not is_none(value):
-                return True
-        return False
-
-    def _check_implicit_return_value(self) -> None:
-        for node in self.returns:
-            if not node.value:
-                self.error_from_node(ImplicitReturnValue, node)
-
-    def _check_unnecessary_return_none(self) -> None:
-        for node in self.returns:
-            if is_none(node.value):
-                self.error_from_node(UnnecessaryReturnNone, node)
-
-    def _check_implicit_return(self, last_node: ast.AST) -> None:
-        if isinstance(last_node, ast.If):
-            if not last_node.body or not last_node.orelse:
-                self.error_from_node(ImplicitReturn, last_node)
-                return
-
-            self._check_implicit_return(last_node.body[-1])
-            self._check_implicit_return(last_node.orelse[-1])
-            return
-
-        if isinstance(last_node, ast.For) and last_node.orelse:
-            self._check_implicit_return(last_node.orelse[-1])
-            return
-
-        if isinstance(last_node, ast.With):
-            self._check_implicit_return(last_node.body[-1])
-            return
-
-        if isinstance(last_node, ast.Assert) and is_false(last_node.test):
-            return
-
-        if not isinstance(
-            last_node, (ast.Return, ast.Raise, ast.While, ast.Try)
-        ):
-            self.error_from_node(ImplicitReturn, last_node)
 
     def _check_unnecessary_assign(self, node: ast.AST) -> None:
         if not isinstance(node, ast.Name):
@@ -193,4 +105,105 @@ class ReturnVisitor(Visitor):
             elif before_assign < lineno:
                 return True
 
+        return False
+
+
+class UnnecessaryReturnNoneMixin(Visitor):
+    def _check_unnecessary_return_none(self) -> None:
+        for node in self.returns:
+            if is_none(node.value):
+                self.error_from_node(UnnecessaryReturnNone, node)
+
+
+class ImplicitReturnValueMixin(Visitor):
+    def _check_implicit_return_value(self) -> None:
+        for node in self.returns:
+            if not node.value:
+                self.error_from_node(ImplicitReturnValue, node)
+
+
+class ImplicitReturnMixin(Visitor):
+    def _check_implicit_return(self, last_node: ast.AST) -> None:
+        if isinstance(last_node, ast.If):
+            if not last_node.body or not last_node.orelse:
+                self.error_from_node(ImplicitReturn, last_node)
+                return
+
+            self._check_implicit_return(last_node.body[-1])
+            self._check_implicit_return(last_node.orelse[-1])
+            return
+
+        if isinstance(last_node, ast.For) and last_node.orelse:
+            self._check_implicit_return(last_node.orelse[-1])
+            return
+
+        if isinstance(last_node, ast.With):
+            self._check_implicit_return(last_node.body[-1])
+            return
+
+        if isinstance(last_node, ast.Assert) and is_false(last_node.test):
+            return
+
+        if not isinstance(
+            last_node, (ast.Return, ast.Raise, ast.While, ast.Try)
+        ):
+            self.error_from_node(ImplicitReturn, last_node)
+
+
+class ReturnVisitor(
+    UnnecessaryAssignMixin,
+    UnnecessaryReturnNoneMixin,
+    ImplicitReturnMixin,
+    ImplicitReturnValueMixin,
+):
+    def __init__(self) -> None:
+        super().__init__()
+        self._stack: List[Any] = []
+
+    @property
+    def returns(self) -> List[ast.Return]:
+        return self._stack[-1][RETURNS]
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
+        self._visit_with_stask(node)
+
+    def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> None:
+        self._visit_with_stask(node)
+
+    def _visit_with_stask(self, node: Function) -> None:
+        self._stack.append(
+            {ASSIGNS: defaultdict(list), REFS: defaultdict(list), RETURNS: []}
+        )
+        self.generic_visit(node)
+        self._check_function(node)
+        self._stack.pop()
+
+    def visit_Return(self, node: ast.Return) -> None:
+        self.returns.append(node)
+        self.generic_visit(node)
+
+    def _check_function(self, node: Function) -> None:
+        if not self.returns or not node.body:
+            return
+
+        if len(node.body) == 1 and isinstance(node.body[-1], ast.Return):
+            # skip functions that consist only `return None`
+            return
+
+        if not self._result_exists():
+            self._check_unnecessary_return_none()
+            return
+
+        self._check_implicit_return_value()
+        self._check_implicit_return(node.body[-1])
+
+        for n in self.returns:
+            if n.value:
+                self._check_unnecessary_assign(n.value)
+
+    def _result_exists(self) -> bool:
+        for node in self.returns:
+            value = node.value
+            if value and not is_none(value):
+                return True
         return False
