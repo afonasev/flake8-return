aa0dc3c8d079c113b9da163c8babfb27f82387ee
Calum Young <calummyoung@gmail.com>
2022-09-24 21:20:34 +0100

    Remove duplicate code

--- flake8_return/stack_keys.py
+++ flake8_return/stack_keys.py
@@ -1,15 +1,3 @@
-import ast
-from typing import Optional
-
-
-def is_none(node: Optional[ast.AST]) -> bool:
-    return isinstance(node, ast.NameConstant) and node.value is None
-
-
-def is_false(node: Optional[ast.AST]) -> bool:
-    return isinstance(node, ast.NameConstant) and node.value is False
-
-
 ASSIGNS = 'assigns'
 REFS = 'refs'
 RETURNS = 'returns'
